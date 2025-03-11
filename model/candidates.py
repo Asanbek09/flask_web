ff467f217f5ef0867cf8d51702cf9211bda25d53
Asanbek09 <samudinua@gmail.com>
2025-03-11 14:27:02 +0600

    candidates.py -> CounselorUser

--- model/candidates.py
+++ model/candidates.py
@@ -22,3 +22,25 @@ class AdminUser:
         self.utype = kwargs.get('utype', 0)
         self.firstname = kwargs.get('firstname', '')
         self.lastname = kwargs.get('lastname', '')
+
+class CounselorUser:
+    id:int
+    age:int
+    position:str
+    prof_started:date
+    username:str
+    password:str
+    utype:int
+    firstname:str
+    lastname:str
+    cid:str
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', 0)
+        self.age = kwargs.get('age', 0)
+        self.position = kwargs.get('position', '')
+        self.prof_started = kwargs.get('prof_started', None)
+        self.username = kwargs.get('username', '')
+        self.password = kwargs.get('password', '')
+        self.utype = kwargs.get('utype', 0)
+        self.firstname = kwargs.get('firstname', '')
+        self.lastname = kwargs.get('lastname', '')
