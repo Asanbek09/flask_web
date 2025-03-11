d14e531dc0c9cd71315482109b2bc2c9184b5b6f
Asanbek09 <samudinua@gmail.com>
2025-03-11 14:16:08 +0600

    Директория model + candidates.py

---
+++ model/candidates.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+class AdminUser:
+    id:int
+    position:str
+    age:int
+    emp_date:date
+    emp_status:str
+    username:str
+    password:str
+    utype:int
+    firstname:str
+    lastname:str
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', 0)
+        self.position = kwargs.get('position', '')
+        self.age = kwargs.get('age', 0)
+        self.emp_date = kwargs.get('emp_date', None)
+        self.emp_status = kwargs.get('emp_status', '')
+        self.username = kwargs.get('username', '')
+        self.password = kwargs.get('password', '')
+        self.utype = kwargs.get('utype', 0)
+        self.firstname = kwargs.get('firstname', '')
+        self.lastname = kwargs.get('lastname', '')
