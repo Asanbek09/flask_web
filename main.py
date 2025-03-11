1e786399e1cd1f86763d10297f10540f5390caaa
Asanbek09 <samudinua@gmail.com>
2025-03-11 10:35:28 +0600

    ain.py -> signup_users_submit

--- main.py
+++ main.py
@@ -14,10 +14,21 @@ def home():
     </body></html>'''
 
 
-@app.route('signup/form', methods=['POST'])
+@app.route('signup/form', methods=['GET'])
 def signup_users_form():
     resp = Response(response= render_template('add_signup.html'), status=200, content_type="text.html")
     return resp
+
+@app.route('/signup/submit', methods=['POST'])
+def signup_users_submit():
+    username = request.form['username']
+    password = request.form['password']
+    user_type = request.form['utype']
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    cid = request.form['cid']
+    insert_signup(username=username, passw=password, utype=user_type, fname=firstname, lname=lastname, cid=cid)
+    return render_template('add_signup_submit.html', message='Add new user!'), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
