89caf3eb3fb43b99fd35659abc1943696ffeb008
Asanbek09 <samudinua@gmail.com>
2025-03-14 10:30:37 +0600

    views/examination.py -> assign_exam

--- views/examination.py
+++ views/examination.py
@@ -1,5 +1,32 @@
 from __main__ import app
 from flask import request, redirect, render_template, url_for, make_response
+from services.utility import list_cid, list_pid, list_qid
+from repository.question_details import insert_question_details
+from services.patient_monitoring import record_patient_exam, list_passing_scores
+from services.exam_management import add_exam_items, list_exam_details
+from uuid import uuid4
+
+@app.route('/exam/assign', methods=['GET', 'POST'])
+def assign_exam():
+    if request.method == 'GET':
+        cids = list_cid()
+        pids = list_pid()
+        response = make_response(render_template('exam/assign_exam_form.html', pids=pids, cids=cids), 200)
+        response.set_cookie('exam_token', str(uuid4()))
+        return response, 200
+    else:
+        id = int(request.form['id'])
+        cid = request.form['cid']
+        pid = int(request.form['pid'])
+        exam_date = request.form['exam_date']
+        duration = int(request.form['duration'])
+        result = insert_question_details(id=id, cid=cid, pid=pid, exam_date=exam_date, duration=duration)
+        if result:
+            task_token = request.cookies.get('exam_token')
+            task = "exam assignment (task id {})".format(task_token)
+            return redirect(url_for('redirect_success_exam', message=task))
+        else:
+            return redirect('/exam/task/error')
 
 @app.route('/exam/score', methods=['GET', 'POST'])
 def record_score():
