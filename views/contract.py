a34c852df3862c35ece528bd819af55932ecc24d
Asanbek09 <samudinua@gmail.com>
2025-03-14 12:05:09 +0600

    contract.py -> ListUnpaidContractView + DeleteContractByPIDView

--- views/contract.py
+++ views/contract.py
@@ -24,3 +24,28 @@ class ContractView(MethodView):
         
         contracts = select_all_patient_contract()
         return render_template("contract/list_patient_contract.html", contracts=contracts)
+    
+class ListUnpaidContractView(View):
+    def dispatch_request(self):
+        contracts = select_all_unpaid_patient()
+        return render_template("contract/list_patient_contract.html", contracts=contracts)
+    
+class DeleteContractByPIDView(View):
+    methods = ['GET', 'POST']
+
+    def __init__(self):
+        pass
+
+    def dispatch_request(self):
+        if request.method == "GET":
+            pids = list_pid()
+            return render_template("contract/delete_patient_contract.html", pids=pids)
+        else:
+            pid = int(request.form['pid'])
+            result = delete_patient_contract_pid(pid)
+            if result == False:
+                pids = list_pid()
+                return render_template("contract/delete_patient_contract.html", pids=pids)
+            
+            contracts = select_all_patient_contract()
+            return render_template("contract/list_patient_contract.html", contracts=contracts)
