f9c653db7b8a3c33a3f7515cdd94227733516a54
Asanbek09 <samudinua@gmail.com>
2025-03-14 11:35:01 +0600

    certificates.py -> show_honor_dissmisal

--- views/certificates.py
+++ views/certificates.py
@@ -16,3 +16,32 @@ def show_certification(name:str, course:str, accomplished_date:date):
         </html>
     """.format(name, course, accomplished_date)
     return certificate, 200
+
+def show_honor_dissmisal(counselor:str, effective_date:date, patient:str):
+    letter = """
+       <html>
+                <head>
+                    <title>Certificate of Accomplishment</title>
+                </head>
+                <body>
+                    <h1>Certificate of Accomplishment</h1>
+                    <p>From: {}
+                    <p>Head, Counselor
+                    <p>Date: {}
+                    <br/>
+                    <p>To: {}
+                    <p>Subject: Termination of consultation
+                    <br/>
+                    <p>Dear {},
+                    <p>&nbsp;&nbsp;It is with deep regret that we are informing you that we have decided to terminate you, 
+                    <p>from our institution. This is for the reason of successive absences and non-complance to the rule of consultancy. 
+                    <p>We also have to acquaint you with the reason for this decision. We wish you all the success in the future. (Cordially describe your greetings and requirements).
+                    <p>Thank you.
+                    <br/>
+
+                    <p>Yours Sincerely,
+                    <p>{}
+                </body>
+            </html>
+    """.format(counselor, effective_date, patient, patient, counselor)
+    return letter, 200
