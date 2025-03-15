a638371f4ae0072187f7eaa9d8450f5b436987c0
Asanbek09 <samudinua@gmail.com>
2025-03-15 13:18:25 +0600

    main.py - небольшие изменения

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from flask import Flask
+import toml
+import json
+from converter.date_converter import DateConverter
 
 app = Flask(__name__, template_folder='pages')
+#app.config.from_file('config.toml', toml.load)
+#app.config.from_file("config.json", load=json.load)
+app.config.from_pyfile('myconfig.py')
+
+app.url_map.converters['date'] = DateConverter
 
 @app.route('/', methods=['GET'])
 def index():
     return "This is an online ... counseling system(OPCS)"
 
-@app.route('/home', methods=['GET'])
-def home():
-    return ''''<html><head><title>Online Personal … System</title></head><body><h1>Online … Counseling System (OPCS)</h1>
-    <p>This is a template of a web-based counseling
-    application where counselors can … … …</em>
-    </body></html>'''
+import views.index
+import views.certificates
+import views.signup
+import views.examination
+import views.reports
+import views.admin
+import views.login
+import views.profile
+from views.contract import ContractView, DeleteContractByPIDView, ListUnpaidContractView
 
-
-@app.route('signup/form', methods=['GET'])
-def signup_users_form():
-    resp = Response(response= render_template('add_signup.html'), status=200, content_type="text.html")
-    return resp
-
-@app.route('/signup/submit', methods=['POST'])
-def signup_users_submit():
-    username = request.form['username']
-    password = request.form['password']
-    user_type = request.form['utype']
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
-    cid = request.form['cid']
-    insert_signup(username=username, passw=password, utype=user_type, fname=firstname, lname=lastname, cid=cid)
-    return render_template('add_signup_submit.html', message='Add new user!'), 200
+app.add_url_rule('/certificate/terminate/<string:counselor>/<date:effective_date>/<string:patient>', 'show_honor_dissmisal', views.certificates.show_honor_dissmisal)
+app.add_url_rule('/contract/patient/add/form', view_func=ContractView.as_view('contract-view'))
+app.add_url_rule('/contract/patient/delete', view_func=DeleteContractByPIDView.as_view('delete-contract-view'))
+app.add_url_rule('/contract/unpaid/patients', view_func=ListUnpaidContractView.as_view('list-unpaid-view'))
 
 if __name__ == '__main__':
     app.run(debug=True)
